046a29a4a3128fefbfa3e7fb033d8daeec94b2d9
Soar Qin <soarchin@gmail.com>
2017-04-01 00:35:21 +0800

    wip

--- scripts/021-openjpeg.sh
+++ scripts/021-openjpeg.sh
@@ -1,2 +1,2 @@
-git_clone https://github.com/uclouvain/openjpeg openjpeg
-compile_with_cmake openjpeg -DBUILD_SHARED_LIBS=Off
+download_file https://github.com/uclouvain/openjpeg/archive/v2.1.2.tar.gz openjpeg-2.1.2
+compile_with_cmake openjpeg-2.1.2 -DBUILD_SHARED_LIBS=Off
