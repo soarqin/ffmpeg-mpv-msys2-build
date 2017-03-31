27a520c83137b16bf7c34ebc9d55c4a791babba8
Soar Qin <soarchin@gmail.com>
2017-04-01 01:44:52 +0800

    wip

--- scripts/001-zlib.sh
+++ scripts/001-zlib.sh
@@ -1,4 +1,4 @@
 download_file http://zlib.net/zlib-1.2.11.tar.xz
-cd $SRC_ROOT/zlib-1.2.11
-echo $BUILD_INSTALL_ROOT
+cd $BUILD_ROOT && rm -rf zlib
+cp -Rf $SRC_ROOT/zlib-1.2.11 ./zlib && cd zlib
 make BINARY_PATH=$BUILD_INSTALL_ROOT/bin INCLUDE_PATH=$BUILD_INSTALL_ROOT/include LIBRARY_PATH=$BUILD_INSTALL_ROOT/lib -f win32/Makefile.gcc $JOBS install
--- scripts/002-bzip2.sh
+++ scripts/002-bzip2.sh
@@ -1,4 +1,5 @@
 download_file http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
-cd $SRC_ROOT/bzip2-1.0.6
+cd $BUILD_ROOT && rm -rf bzip2
+cp -Rf $SRC_ROOT/bzip2-1.0.6 ./bzip2 && cd bzip2
 sed -i -e "/PREFIX=/d" Makefile
 make PREFIX=$BUILD_INSTALL_ROOT $JOBS install
