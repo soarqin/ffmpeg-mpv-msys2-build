d224df0de82b6905d8ce5eb774787ced5eb1cf9f
Soar Qin <soarchin@gmail.com>
2017-04-13 01:44:24 +0800

    fixes

--- scripts/117-vid-stab.sh
+++ scripts/117-vid-stab.sh
@@ -1,7 +1,10 @@
 if [ "x${VIDSTAB_VERSION}" == "xgit" ]; then
     git_clone https://github.com/georgmartius/vid.stab vid.stab-${VIDSTAB_VERSION}
 else
-    download_file https://github.com/georgmartius/vid.stab/archive/${VIDSTAB_VERSION}.tar.gz
+    download_file https://github.com/georgmartius/vid.stab/archive/${VIDSTAB_VERSION}.tar.gz vid.stab-${VIDSTAB_VERSION}
+    pushd ${SRC_ROOT}/vid.stab-${VIDSTAB_VERSION}
+    sed -i -e 's,vidstab SHARED ,vidstab STATIC ,g' CMakeLists.txt
+    popd
 fi
 if [ $result == true ]; then return; fi
-compile_with_cmake vid.stab-${VIDSTAB_VERSION} -DUSE_OMP=Off -DBUILD_SHARED_LIBS=Off
+compile_with_cmake vid.stab-${VIDSTAB_VERSION} -DUSE_OMP=OFF -DBUILD_SHARED_LIBS=OFF
