d40044d60293a70f47fe3d737a0f7712c37135ca
Soar Qin <soarchin@gmail.com>
2017-04-14 19:53:42 +0800

    netcdf git fix

--- scripts/135-netcdf.sh
+++ scripts/135-netcdf.sh
@@ -4,4 +4,9 @@ else
     download_file https://github.com/Unidata/netcdf-c/archive/v${NETCDF_VERSION}.tar.gz netcdf-c-${NETCDF_VERSION}
 fi
 if [ $result == true ]; then return; fi
+pushd ${SRC_ROOT}/netcdf-c-${NETCDF_VERSION}
+if [ -f libdispatch/utf8proc.h ]; then
+    sed -i -e 's,#ifdef _WIN32,#if 0,g' libdispatch/utf8proc.h
+fi
+popd
 compile_with_configure netcdf-c-${NETCDF_VERSION} --enable-static --disable-shared --disable-testsets --disable-utilities --disable-dap --disable-netcdf-4
