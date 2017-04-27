10e843e26cae55caa3091dd9ded8b0f978195061
Soar Qin <soarchin@gmail.com>
2017-04-28 01:21:23 +0800

    missing line

--- scripts/deps/115-mpg123.sh
+++ scripts/deps/115-mpg123.sh
@@ -3,6 +3,7 @@ if [ "x${MPG123_VERSION}" == "xsvn" ]; then
 else
     download_file https://sourceforge.net/projects/mpg123/files/mpg123/${MPG123_VERSION}/mpg123-${MPG123_VERSION}.tar.bz2/download
 fi
+if [ $result == true ]; then return; fi
 pushd ${SRC_ROOT}/mpg123-${MPG123_VERSION}
 if [ "x$(grep Libs.private libmpg123.pc.in)" == "x" ]; then
     sed -i -s 's,Libs: -L${libdir} -lmpg123,Libs: -L${libdir} -lmpg123\nLibs.private: -lshlwapi ,g' libmpg123.pc.in
