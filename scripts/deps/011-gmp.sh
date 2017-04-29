bcdee0bdd45cd056b5b9d32c3867da8959703b3b
Soar Qin <soarchin@gmail.com>
2017-04-29 22:12:11 +0800

    1.fixed problem cased by msys2 installed guile 2.Used gnu download address for gmplib

--- scripts/deps/011-gmp.sh
+++ scripts/deps/011-gmp.sh
@@ -1,3 +1,3 @@
-download_file https://gmplib.org/download/gmp/gmp-${GMP_VERSION}.tar.xz
+download_file https://ftp.gnu.org/gnu/gmp/gmp-${GMP_VERSION}.tar.xz
 if [ $result == true ]; then return; fi
 compile_with_configure gmp-${GMP_VERSION} --enable-static --disable-shared
