8ba4cac289f3abc51d15130aaf9be50e3b64e53e
Soar Qin <soarchin@gmail.com>
2017-05-05 14:03:10 +0800

    minor fixes

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -6,7 +6,7 @@ PATCH_ROOT=$(realpath ${SCRIPT_ROOT}/../patches)
 set -e
 
 if [ "x${NO_PKG_INSTALL}" == "x" ]; then
-    pacman -Syq --noconfirm --needed autoconf autogen automake libtool m4 bison flex gzip bzip2 xz tar ca-certificates curl dos2unix doxygen texinfo gettext git mercurial subversion patch gawk glib2 gperf grep info make nasm yasm pkg-config python ${ARCH_PREFIX}toolchain ${ARCH_PREFIX}cmake ${ARCH_PREFIX}python2-lxml ${ARCH_PREFIX}python2-six
+    pacman -Syuq --noconfirm --needed autoconf autogen automake libtool m4 bison flex gzip bzip2 xz tar ca-certificates curl dos2unix doxygen texinfo gettext git mercurial subversion patch gawk glib2 gperf grep info make nasm yasm pkg-config python ${ARCH_PREFIX}toolchain ${ARCH_PREFIX}cmake ${ARCH_PREFIX}python2-lxml ${ARCH_PREFIX}python2-six
 fi
 
 cd ${SCRIPT_ROOT}
