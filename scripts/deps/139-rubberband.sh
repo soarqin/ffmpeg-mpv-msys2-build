3101f8ca29869b6ce735d0fe347080c0ee6c3049
Soar Qin <soarchin@gmail.com>
2017-05-16 00:02:47 +0800

    fix

--- scripts/deps/139-rubberband.sh
+++ scripts/deps/139-rubberband.sh
@@ -1,7 +1,10 @@
+download_file http://code.breakfastquay.com/attachments/download/34/rubberband-${RUBBERBAND_VERSION}.tar.bz2
+
+if [ $result == true ]; then return; fi
+
 #INSTALL LADSPA Plugin Header
 curl -LJ http://www.ladspa.org/ladspa_sdk/ladspa.h.txt > ${BUILD_INSTALL_ROOT}/include/ladspa.h
 
-download_file http://code.breakfastquay.com/attachments/download/34/rubberband-${RUBBERBAND_VERSION}.tar.bz2
 pushd $SRC_ROOT/rubberband-${RUBBERBAND_VERSION}
 if [ ! -f ./patched ]; then
     patch -p1 < ${PATCH_ROOT}/139-rubberband.patch
@@ -9,5 +12,4 @@ if [ ! -f ./patched ]; then
 fi
 popd
 
-if [ $result == true ]; then return; fi
 compile_with_configure_dirty rubberband-${RUBBERBAND_VERSION}
