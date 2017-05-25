5611778211385851825477780528b24800cd36f3
Soar Qin <soarchin@gmail.com>
2017-05-25 16:52:29 +0800

    fixed patch script for libgme

--- scripts/deps/123-libgme.sh
+++ scripts/deps/123-libgme.sh
@@ -1,14 +1,13 @@
 if [ "x${GME_VERSION}" == "xgit" ]; then
     git_clone https://bitbucket.org/mpyne/game-music-emu game-music-emu-${GME_VERSION}
-    rm -f ${SRC_ROOT}/game-music-emu-${GME_VERSION}/patched
 else
     download_file https://bitbucket.org/mpyne/game-music-emu/downloads/game-music-emu-${GME_VERSION}.tar.bz2
+    pushd ${SRC_ROOT}/game-music-emu-${GME_VERSION}
+    if [ ! -f ./patched ]; then
+        patch -p1 < ${PATCH_ROOT}/123-libgme.patch
+        touch ./patched
+    fi
+    popd
 fi
 if [ $result == true ]; then return; fi
-pushd ${SRC_ROOT}/game-music-emu-${GME_VERSION}
-if [ ! -f ./patched ]; then
-    patch -p1 < ${PATCH_ROOT}/123-libgme.patch
-    touch ./patched
-fi
-popd
 compile_with_cmake game-music-emu-${GME_VERSION} -DBUILD_SHARED_LIBS=OFF -DENABLE_UBSAN=OFF
