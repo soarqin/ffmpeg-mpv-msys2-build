ffd0484081b15d0811b871d6289c345ff13bbe4f
Soar Qin <soarchin@gmail.com>
2017-06-03 20:11:19 +0800

    only apply patch to dev version of libarchive

--- scripts/mpv/005-libarchive.sh
+++ scripts/mpv/005-libarchive.sh
@@ -1,8 +1,9 @@
 if [ "x${LIBARCHIVE_VERSION}" == "xgit" ]; then
     git_clone https://github.com/libarchive/libarchive libarchive-${LIBARCHIVE_VERSION}
+    if [ $result == true ]; then return; fi
+    patch_source libarchive-${LIBARCHIVE_VERSION} 005-libarchive.patch
 else
     download_file https://github.com/libarchive/libarchive/archive/v${LIBARCHIVE_VERSION}.tar.gz libarchive-${LIBARCHIVE_VERSION}
 fi
 if [ $result == true ]; then return; fi
-patch_source libarchive-${LIBARCHIVE_VERSION} 005-libarchive.patch
 compile_with_configure libarchive-${LIBARCHIVE_VERSION} --enable-static --disable-shared --disable-rpath --without-iconv --disable-bsdtar --disable-bsdcat --disable-bsdcpio --without-lz4 --without-openssl --without-xml2 --without-expat --without-cng
