d27f8dc04def174bc3e851ff62197e554f44b6e6
Soar Qin <soarchin@gmail.com>
2017-06-07 10:54:16 +0800

    1. install mpv to dist dir 2. updated ffmpeg to 3.3.2

--- scripts/mpv/998-mpv.sh
+++ scripts/mpv/998-mpv.sh
@@ -8,11 +8,9 @@ sed -i -e "/^${fnprefix} /d" "${FINISHED}"
 export PKG_CONFIG_PATH=$(realpath ${SCRIPT_ROOT}/../dist/${ARCH}_mpv${BUILD_EXTRA_SUFFIX}/lib/pkgconfig):${PKG_CONFIG_PATH}
 pushd ${SRC_ROOT}/mpv-${MPV_VERSION}
 ./bootstrap.py
-python3 waf configure --prefix=${BUILD_INSTALL_ROOT} -o ${BUILD_ROOT}/mpv-${MPV_VERSION} --enable-static-build --disable-manpage-build --enable-dvdread --enable-dvdnav --enable-libarchive
+python3 waf configure --prefix=$(realpath ${SCRIPT_ROOT}/../dist/${ARCH}_mpv${BUILD_EXTRA_SUFFIX}) -o ${BUILD_ROOT}/mpv-${MPV_VERSION} --enable-static-build --disable-manpage-build --enable-dvdread --enable-dvdnav --enable-libarchive
 replace_str="LIBPATH_lib\1 = ['${LOCALDESTDIR}/lib','${MINGW_PREFIX}/lib']"
 sed -r -i "s:LIBPATH_lib(ass|av(|device|filter)) = .*:${replace_str}:g" ${BUILD_ROOT}/mpv-${MPV_VERSION}/c4che/_cache.py
 python3 waf build ${MAKE_JOBS}
-popd
-pushd ${BUILD_ROOT}/mpv-${MPV_VERSION}
-strip ./mpv.exe
+python3 waf install
 popd
