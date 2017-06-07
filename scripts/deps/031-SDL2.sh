5c12636bce4f68499a7e4c4a5ba059c0b7834a0b
Soar Qin <soarchin@gmail.com>
2017-06-07 13:40:38 +0800

    1. patch fix for SDL2 2. more patches borrowed from MINGW packages

--- scripts/deps/031-SDL2.sh
+++ scripts/deps/031-SDL2.sh
@@ -2,7 +2,8 @@ if [ "x${SDL2_VERSION}" == "xhg" ]; then
     hg_clone http://hg.libsdl.org/SDL SDL2-${SDL2_VERSION}
 else
     download_file http://www.libsdl.org/release/SDL2-${SDL2_VERSION}.tar.gz
+    if [ $result == true ]; then return; fi
+    patch_source SDL2-${SDL2_VERSION} 031-SDL2.patch
 fi
 if [ $result == true ]; then return; fi
-patch_source SDL2-${SDL2_VERSION} 031-SDL2.patch
 compile_with_configure SDL2-${SDL2_VERSION} --enable-static --disable-shared --disable-video-x11-xinput
--- scripts/deps/116-libcaca.sh
+++ scripts/deps/116-libcaca.sh
@@ -1,10 +1,11 @@
 if [ "x${CACA_VERSION}" == "xgit" ]; then
     git_clone https://github.com/cacalabs/libcaca libcaca-${CACA_VERSION}
     if [ $result == true ]; then return; fi
-    patch_source libcaca-${CACA_VERSION} 116-libcaca-git.patch
+    patch_source libcaca-${CACA_VERSION} 116-libcaca.patch
 else
     download_file https://github.com/cacalabs/libcaca/archive/v${CACA_VERSION}.tar.gz libcaca-${CACA_VERSION}
     if [ $result == true ]; then return; fi
     patch_source libcaca-${CACA_VERSION} 116-libcaca.patch
+    patch_source libcaca-${CACA_VERSION} 116-libcaca-2.patch
 fi
 compile_with_configure libcaca-${CACA_VERSION} --enable-static --disable-shared --disable-ncurses --disable-doc
