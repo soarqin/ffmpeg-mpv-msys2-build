59766ff7cfecc966632cc6aa566e22e3379dd3cb
Soar Qin <soarchin@gmail.com>
2017-06-07 12:32:39 +0800

    fixed LAME patch

--- scripts/deps/111-LAME.sh
+++ scripts/deps/111-LAME.sh
@@ -1,9 +1,4 @@
 download_file https://sourceforge.net/projects/lame/files/lame/3.99/lame-${LAME_VERSION}.tar.gz/download
 if [ $result == true ]; then return; fi
-pushd $SRC_ROOT/lame-${LAME_VERSION}
-if [ ! -f configure.orig ]; then
-    cp configure configure.orig
-    patch -p0 < $PATCH_ROOT/111-LAME-000-xmmintrin.patch
-fi
-popd
+patch_source lame-${LAME_VERSION} 111-LAME-000-xmmintrin.patch
 compile_with_configure lame-${LAME_VERSION} --enable-static --disable-shared --disable-rpath --disable-frontend --enable-nasm
