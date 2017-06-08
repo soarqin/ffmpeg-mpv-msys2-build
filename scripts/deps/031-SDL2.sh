7ef21fc1b03e877b6e280a11c2a98d144acca90f
Soar Qin <soarchin@gmail.com>
2017-06-08 12:39:28 +0800

    fixed SDL2 patch

--- scripts/deps/031-SDL2.sh
+++ scripts/deps/031-SDL2.sh
@@ -1,9 +1,11 @@
 if [ "x${SDL2_VERSION}" == "xhg" ]; then
     hg_clone http://hg.libsdl.org/SDL SDL2-${SDL2_VERSION}
+    if [ $result == true ]; then return; fi
+    patch_source SDL2-${SDL2_VERSION} 031-SDL2.patch
 else
     download_file http://www.libsdl.org/release/SDL2-${SDL2_VERSION}.tar.gz
     if [ $result == true ]; then return; fi
     patch_source SDL2-${SDL2_VERSION} 031-SDL2.patch
+    patch_source SDL2-${SDL2_VERSION} 031-SDL2-2.patch
 fi
-if [ $result == true ]; then return; fi
 compile_with_configure SDL2-${SDL2_VERSION} --enable-static --disable-shared --disable-video-x11-xinput
