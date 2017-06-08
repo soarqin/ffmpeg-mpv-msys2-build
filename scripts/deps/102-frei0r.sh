d712c68319b9bc53181ecd6e5e5142a6bb74804d
Soar Qin <soarchin@gmail.com>
2017-06-09 00:41:42 +0800

    don't use https for frei0r

--- scripts/deps/102-frei0r.sh
+++ scripts/deps/102-frei0r.sh
@@ -1,7 +1,7 @@
 if [ "x${FREI0R_VERSION}" == "xgit" ]; then
-    git_clone https://code.dyne.org/frei0r frei0r-plugins-${FREI0R_VERSION}
+    git_clone http://code.dyne.org/frei0r frei0r-plugins-${FREI0R_VERSION}
 else
-    download_file https://files.dyne.org/frei0r/frei0r-plugins-${FREI0R_VERSION}.tar.gz
+    download_file http://files.dyne.org/frei0r/frei0r-plugins-${FREI0R_VERSION}.tar.gz
 fi
 if [ $result == true ]; then return; fi
 pushd ${SRC_ROOT}/frei0r-plugins-${FREI0R_VERSION}
