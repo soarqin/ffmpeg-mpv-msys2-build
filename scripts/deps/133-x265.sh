004b855fa25055cace93d76225133daef5b4adb7
Soar Qin <soarchin@gmail.com>
2017-06-09 20:30:51 +0800

    add 10/12 bits x265 libraries combined into 8bit one

--- scripts/deps/133-x265.sh
+++ scripts/deps/133-x265.sh
@@ -1,10 +1,6 @@
 if [ "x${X265_VERSION}" == "xhg" ]; then
     hg_clone https://bitbucket.org/multicoreware/x265 x265_${X265_VERSION}
-else
-    download_file https://bitbucket.org/multicoreware/x265/downloads/x265_${X265_VERSION}.tar.gz
-fi
-if [ $result == true ]; then return; fi
-if [ "x${X265_VERSION}" == "xhg" ]; then
+    if [ $result == true ]; then return; fi
     pushd $SRC_ROOT/x265_${X265_VERSION}
     hg log -r0 --template "repo: {node}\n" > .hg_archival.txt
     hg log -r. --template "node: {node}\n" >> .hg_archival.txt
@@ -12,10 +8,41 @@ if [ "x${X265_VERSION}" == "xhg" ]; then
     hg log -r. --template "latesttag: {latesttag}\n" >> .hg_archival.txt
     hg log -r. --template "latesttagdistance: {latesttagdistance}\n" >> .hg_archival.txt
     popd
+else
+    download_file https://bitbucket.org/multicoreware/x265/downloads/x265_${X265_VERSION}.tar.gz
 fi
+if [ $result == true ]; then return; fi
+
 mkdir -p $BUILD_ROOT/x265_${X265_VERSION}
 pushd $BUILD_ROOT/x265_${X265_VERSION}
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$BUILD_INSTALL_ROOT -DENABLE_CLI=Off -DENABLE_PIC=On -DENABLE_SHARED=Off $SRC_ROOT/x265_${X265_VERSION}/source
+
+mkdir -p 8bit 10bit 12bit
+cd 12bit
+cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=OFF -DENABLE_PIC=ON -DMAIN12=ON $SRC_ROOT/x265_${X265_VERSION}/source
 make ${MAKE_JOBS}
+cp libx265.a ../8bit/libx265_main12.a
+
+cd ../10bit
+cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=OFF -DENABLE_PIC=ON $SRC_ROOT/x265_${X265_VERSION}/source
+make ${MAKE_JOBS}
+cp libx265.a ../8bit/libx265_main10.a
+
+cd ../8bit
+cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$BUILD_INSTALL_ROOT -DEXTRA_LIB="x265_main10.a;x265_main12.a" -DEXTRA_LINK_FLAGS=-L. -DLINKED_10BIT=ON -DLINKED_12BIT=ON -DENABLE_CLI=OFF -DENABLE_PIC=ON -DENABLE_SHARED=OFF $SRC_ROOT/x265_${X265_VERSION}/source
+make ${MAKE_JOBS}
+
+# rename the 8bit library, then combine all three into libx265.a using GNU ar
+mv libx265.a libx265_main.a
+
+ar -M <<EOF
+CREATE libx265.a
+ADDLIB libx265_main.a
+ADDLIB libx265_main10.a
+ADDLIB libx265_main12.a
+SAVE
+END
+EOF
+
 make install
+
 popd
