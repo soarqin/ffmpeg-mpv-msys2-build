5592c33ec50f1556b8474af8e00a6ca2bf6d33f4
Soar Qin <soarchin@gmail.com>
2017-06-11 00:07:27 +0800

    try to build both 8/10bits x264, in shared mode

--- scripts/deps/131-x264.sh
+++ scripts/deps/131-x264.sh
@@ -3,4 +3,17 @@ if [ $result == true ]; then return; fi
 if [ "x$ARCH" == "x32" ]; then
     ECFLAGS="--host=i686-w64-mingw32"
 fi
-compile_with_configure x264-git-${X264_BRANCH} --enable-static --disable-cli --enable-pic $ECFLAGS
+rm -rf ${BUILD_ROOT}/x264-git-${X264_BRANCH}
+mkdir -p ${BUILD_ROOT}/x264-git-${X264_BRANCH}
+pushd ${BUILD_ROOT}/x264-git-${X264_BRANCH}
+${SRC_ROOT}/x264-git-${X264_BRANCH}/configure --prefix=/usr --enable-shared --disable-cli --bit-depth=10 --enable-pic $ECFLAGS
+make ${MAKE_JOBS}
+make install DESTDIR=$(realpath -m ${SCRIPT_ROOT}/../dist/${ARCH}_x264-10bit${BUILD_EXTRA_SUFFIX})
+popd
+compile_with_configure x264-git-${X264_BRANCH} --enable-shared --disable-cli --enable-pic $ECFLAGS
+pushd ${BUILD_INSTALL_ROOT}/lib
+gendef ../bin/libx264-*.dll
+sed -i -e 's/^\(x264_.*\..*\)$/"\1"/g' libx264-*.def
+dlltool --def libx264-*.def --output-delaylib libx264.a
+rm -f libx264-*.def
+popd
