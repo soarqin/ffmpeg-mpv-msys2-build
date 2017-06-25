54a0ddc90e2839c4402a66ddfefe4b25727e38c7
Soar Qin <soarchin@gmail.com>
2017-06-26 00:33:03 +0800

    use openjpeg git HEAD for repo build

--- scripts/deps/021-openjpeg.sh
+++ scripts/deps/021-openjpeg.sh
@@ -1,4 +1,10 @@
-download_file https://github.com/uclouvain/openjpeg/archive/v${OPENJPEG_VERSION}.tar.gz openjpeg-${OPENJPEG_VERSION}
-if [ $result == true ]; then return; fi
-patch_source openjpeg-${OPENJPEG_VERSION} 021-openjpeg.patch
+if [ "x${OPENJPEG_VERSION}" == "xgit" ]; then
+    git_clone https://github.com/uclouvain/openjpeg openjpeg-${OPENJPEG_VERSION}
+    if [ $result == true ]; then return; fi
+    patch_source openjpeg-${OPENJPEG_VERSION} 021-openjpeg-git.patch
+else
+    download_file https://github.com/uclouvain/openjpeg/archive/v${OPENJPEG_VERSION}.tar.gz openjpeg-${OPENJPEG_VERSION}
+    if [ $result == true ]; then return; fi
+    patch_source openjpeg-${OPENJPEG_VERSION} 021-openjpeg.patch
+fi
 compile_with_cmake openjpeg-${OPENJPEG_VERSION} -DBUILD_SHARED_LIBS=Off -DBUILD_CODEC=Off
