0d845208acf0eee6ce8c11eab6e69bf91fc4abf0
Soar Qin <soarchin@gmail.com>
2017-06-27 19:52:12 +0800

    upgrade libopus to 1.2.1

--- scripts/mpv/998-mpv.sh
+++ scripts/mpv/998-mpv.sh
@@ -8,9 +8,9 @@ sed -i -e "/^${fnprefix} /d" "${FINISHED}"
 export PKG_CONFIG_PATH=$(realpath -m ${SCRIPT_ROOT}/../dist/${ARCH}_mpv${BUILD_EXTRA_SUFFIX}/lib/pkgconfig):${PKG_CONFIG_PATH}
 pushd ${SRC_ROOT}/mpv-${MPV_VERSION}
 ./bootstrap.py
-LDFLAGS='-Wl,-Bstatic -lpthread' python3 waf configure --prefix=$(realpath -m ${SCRIPT_ROOT}/../dist/${ARCH}_mpv${BUILD_EXTRA_SUFFIX}) -o ${BUILD_ROOT}/mpv-${MPV_VERSION} --enable-static-build --disable-manpage-build --enable-dvdread --enable-dvdnav --enable-libarchive
+LDFLAGS='-Wl,-Bstatic -lpthread' /usr/bin/python2 waf configure --prefix=$(realpath -m ${SCRIPT_ROOT}/../dist/${ARCH}_mpv${BUILD_EXTRA_SUFFIX}) -o ${BUILD_ROOT}/mpv-${MPV_VERSION} --enable-static-build --disable-manpage-build --enable-dvdread --enable-dvdnav --enable-libarchive
 replace_str="LIBPATH_lib\1 = ['${LOCALDESTDIR}/lib','${MINGW_PREFIX}/lib']"
 sed -r -i "s:LIBPATH_lib(ass|av(|device|filter)) = .*:${replace_str}:g" ${BUILD_ROOT}/mpv-${MPV_VERSION}/c4che/_cache.py
-python3 waf build ${MAKE_JOBS}
-python3 waf install
+/usr/bin/python2 waf build ${MAKE_JOBS}
+/usr/bin/python2 waf install
 popd
