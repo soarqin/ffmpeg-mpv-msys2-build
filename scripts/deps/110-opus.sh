0a678c685cc161318d1899bdccbe2edcae213a11
Soar Qin <soarchin@gmail.com>
2017-06-28 10:23:54 +0800

    fixed opus download url

--- scripts/deps/110-opus.sh
+++ scripts/deps/110-opus.sh
@@ -1,7 +1,7 @@
 if [ "x${OPUS_VERSION}" == "xgit" ]; then
     git_clone https://git.xiph.org/opus.git opus-${OPUS_VERSION}
 else
-    download_file http://downloads.xiph.org/releases/opus/opus-${OPUS_VERSION}.tar.gz
+    download_file https://archive.mozilla.org/pub/opus/opus-${OPUS_VERSION}.tar.gz
 fi
 if [ $result == true ]; then return; fi
 compile_with_configure opus-${OPUS_VERSION} --enable-static --disable-shared --disable-doc --disable-extra-programs --enable-custom-modes
