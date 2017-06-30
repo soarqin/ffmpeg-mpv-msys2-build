143bb65590be5a15191e8ec3443b575c46facb48
Soar Qin <soarchin@gmail.com>
2017-06-30 23:46:50 +0800

    rebuild ffmpeg for mpv

--- scripts/mpv/997-ffmpeg.sh
+++ scripts/mpv/997-ffmpeg.sh
@@ -4,7 +4,7 @@ if [ "x${FFMPEG_VERSION}" == "xgit" ]; then
 else
     download_file http://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.bz2
 fi
-if [ $result == true ]; then return; fi
+if [ $any_dirty != true ]; then return; fi
 sed -i -e "/^${fnprefix} /d" "${FINISHED}"
 
 rm -Rf ${BUILD_ROOT}/ffmpeg-${FFMPEG_VERSION}
