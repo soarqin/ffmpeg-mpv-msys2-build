c0d2576d34e98fa16d1f246cfdd4aeb38b6899c0
Soar Qin <soarchin@gmail.com>
2017-07-02 21:00:07 +0800

    sciprt fixes

--- scripts/deps/004-snappy.sh
+++ scripts/deps/004-snappy.sh
@@ -1,7 +1,7 @@
 if [ "x${SNAPPY_VERSION}" == "xgit" ]; then
     git_clone https://github.com/google/snappy snappy-${SNAPPY_VERSION}
 else
-    download_file https://github.com/google/snappy/releases/download/${SNAPPY_VERSION}/snappy-${SNAPPY_VERSION}.tar.gz
+    download_file https://github.com/google/snappy/archive/${SNAPPY_VERSION}.tar.gz snappy-${SNAPPY_VERSION}
 fi
 if [ "$result" = true ]; then return; fi
 patch_source snappy-${SNAPPY_VERSION} 004-snappy.patch
