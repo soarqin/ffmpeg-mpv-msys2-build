0fa6aa5bd7dd448184c54d271730b01f69e9f0df
Soar Qin <soarchin@gmail.com>
2017-07-30 23:09:27 +0800

    updated soxr repo url

--- scripts/deps/113-soxr.sh
+++ scripts/deps/113-soxr.sh
@@ -1,3 +1,7 @@
-git_clone https://github.com/chirlu/soxr soxr-git
+if [ "x${SOXR_VERSION}" == "xgit" ]; then
+    git_clone https://git.code.sf.net/p/soxr/code soxr-${SOXR_VERSION}
+else
+    download_file https://sourceforge.net/projects/soxr/files/soxr-${SOXR_VERSION}-Source.tar.xz/download soxr-${SOXR_VERSION}
+fi
 if [ "$result" = true ]; then return; fi
-compile_with_cmake soxr-git -DBUILD_SHARED_LIBS=Off -DBUILD_TESTS=Off -DWITH_OPENMP=Off
+compile_with_cmake soxr-${SOXR_VERSION} -DBUILD_SHARED_LIBS=Off -DBUILD_TESTS=Off -DWITH_OPENMP=Off
