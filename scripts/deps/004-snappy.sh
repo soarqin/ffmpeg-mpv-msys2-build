1cead22f786d2b9a1ed87a3e0f831db8ca419659
Soar Qin <soarchin@gmail.com>
2017-09-03 17:32:23 +0800

    use cmake for snappy build

--- scripts/deps/004-snappy.sh
+++ scripts/deps/004-snappy.sh
@@ -4,11 +4,4 @@ else
     download_file https://github.com/google/snappy/archive/${SNAPPY_VERSION}.tar.gz snappy-${SNAPPY_VERSION}
 fi
 if [ "$result" = true ]; then return; fi
-patch_source snappy-${SNAPPY_VERSION} 004-snappy.patch
-auto_reconf snappy-${SNAPPY_VERSION}
-pushd $SRC_ROOT/snappy-${SNAPPY_VERSION}
-if [ ! -f ./README ]; then
-    touch ./README
-fi
-popd
-compile_with_configure snappy-${SNAPPY_VERSION} --enable-static --disable-shared
+compile_with_cmake snappy-${SNAPPY_VERSION} -DBUILD_SHARED_LIBS=OFF -DSNAPPY_BUILD_TESTS=OFF
