fe6da34e289584d9096e8194e9bc0b7daa36d54b
Soar Qin <soarchin@gmail.com>
2017-11-18 15:33:40 +0800

    updated kvazaar

--- scripts/deps/132-kvazaar.sh
+++ scripts/deps/132-kvazaar.sh
@@ -1,8 +1,8 @@
 if [ "x${KVAZAAR_VERSION}" == "xgit" ]; then
     git_clone https://github.com/ultravideo/kvazaar kvazaar-${KVAZAAR_VERSION}
 else
-    download_file https://github.com/ultravideo/kvazaar/releases/download/v${KVAZAAR_VERSION}/kvazaar-v${KVAZAAR_VERSION}.tar.gz kvazaar-${KVAZAAR_VERSION}
+    download_file https://github.com/ultravideo/kvazaar/releases/download/v${KVAZAAR_VERSION}/kvazaar-${KVAZAAR_VERSION}.tar.xz kvazaar-${KVAZAAR_VERSION}
 fi
 if [ "$result" = true ]; then return; fi
 patch_source kvazaar-${KVAZAAR_VERSION} 132-kvazaar.patch
-compile_with_configure kvazaar-${KVAZAAR_VERSION} --enable-static --disable-shared --disable-werror
+compile_with_configure kvazaar-${KVAZAAR_VERSION} --enable-static --disable-shared
